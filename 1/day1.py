5ca33b40ee22ef1efb29de283f1cdc99ad254062
Forrest <forforr@gmail.com>
2019-12-04 18:17:28 -0800

    Day 1

---
+++ 1/day1.py
@@ -0,0 +1,21 @@
+import math
+
+# to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
+def calculatefuel(mass):
+	if mass < 6:
+		return 0
+
+	fuelneeded = math.floor(mass / 3) - 2
+		
+	if fuelneeded > 6:
+		fuelneeded += calculatefuel(fuelneeded)
+	
+	return fuelneeded
+
+total = 0
+with open('input.txt') as fp:
+	for i, line in enumerate(fp):
+		line = int(line)
+		total += calculatefuel(line)
+
+print(total)
