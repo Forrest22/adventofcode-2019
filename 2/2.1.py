dd2c7c5b25859810ac9ba0bc8abe2252bf36513f
Forrest <forforr@gmail.com>
2019-12-07 12:27:32 -0800

    Finished day 2

--- 2/2.1.py
+++ 2/2.1.py
@@ -1,5 +1,6 @@
 import csv
 
+# given opcode and 2 resulting values calculates and the returns result
 def calculateresult(opcode, var1, var2):
 	# print(opcode,var1,var2)
 	if opcode == 1:
@@ -10,8 +11,9 @@ def calculateresult(opcode, var1, var2):
 		raise Exception("Opcode \"" + str(opcode) + "\" is funky!")
 	return result
 
+#readsd the opcode at position, calculates the result, and stores the value
 def evalinstruction(instruc, position):
-	print(instruc)
+	# print(instruc)
 	opcode = int(instruc[position])
 	var1 = instruc[int(instruc[position+1])]
 	var2 = instruc[int(instruc[position+2])]
@@ -20,6 +22,7 @@ def evalinstruction(instruc, position):
 	instruc[resultpos] = calculateresult(opcode, var1, var2)
 	return instruc
 
+# reads the values
 instructions = []
 with open('input.txt') as fp:
 	reader = csv.reader(fp)
@@ -29,14 +32,16 @@ with open('input.txt') as fp:
 instructions[1] = 12
 instructions[2] = 2
 
+#loops through opcode positions
 position = 0
 while position < len(instructions) - 4:
-	print(instructions[position], instructions[position+1], instructions[position+2], instructions[position+3])
+	# print(instructions[position], instructions[position+1], instructions[position+2], instructions[position+3])
 	if instructions[position] != 99:
-		print("not 99")
+		# print("not 99")
 		storeinpos = int(instructions[position+3])
 		instructions = evalinstruction(instructions, position)
 
 	position += 4
 
+# prints the answer
 print(instructions[0])
--- 2/2.2.py
+++ 2/2.2.py
@@ -1,42 +1,83 @@
 import csv
 
-def calculateresult(opcode, var1, var2):
-	# print(opcode,var1,var2)
+# given opcode and 2 resulting values calculates and the returns result
+def calculateresult(opcode, parm1, parm2):
+	# print(opcode,parm1,parm2)
 	if opcode == 1:
-		result = var1 + var2
+		result = parm1 + parm2
 	elif opcode == 2:
-		result = var1 * var2
+		result = parm1 * parm2
 	else:
 		raise Exception("Opcode \"" + str(opcode) + "\" is funky!")
 	return result
 
+#readsd the opcode at position, calculates the result, and stores the value
 def evalinstruction(instruc, position):
-	print(instruc)
+	# print(instruc)
 	opcode = int(instruc[position])
-	var1 = instruc[int(instruc[position+1])]
-	var2 = instruc[int(instruc[position+2])]
+	parm1 = instruc[int(instruc[position+1])]
+	parm2 = instruc[int(instruc[position+2])]
 	resultpos = instruc[position+3]
 
-	instruc[resultpos] = calculateresult(opcode, var1, var2)
+	instruc[resultpos] = calculateresult(opcode, parm1, parm2)
 	return instruc
 
-instructions = []
-with open('input.txt') as fp:
-	reader = csv.reader(fp)
-	instructions = [int(i) for i in list(reader)[0]]
+# reads the values
+def getinstructions():
+	instructions = []
+	with open('input.txt') as fp:
+		reader = csv.reader(fp)
+		instructions = [int(i) for i in list(reader)[0]]
+	return instructions
 
-# replace position 1 with the value 12 and replace position 2 with the value 2
-instructions[1] = 12
-instructions[2] = 2
+# get noun and verb
+def getnoun(n=False):
+	# print (n)
+	if isinstance(n,int):
+		return n
+	else:
+		print("Noun:")
+		noun = input()
+		return noun
 
-position = 0
-while position < len(instructions) - 4:
-	print(instructions[position], instructions[position+1], instructions[position+2], instructions[position+3])
-	if instructions[position] != 99:
-		print("not 99")
-		storeinpos = int(instructions[position+3])
-		instructions = evalinstruction(instructions, position)
+def getverb(v=False):
+	# print(v)
+	if isinstance(v,int):
+		return v
+	else:
+		print("Verb:")
+		verb = input()
+		return verb
 
-	position += 4
+def runinstructions(n = False, v = False):
+	# print(n,v)
+	instructions = getinstructions()
+	noun = getnoun(n)
+	verb = getverb(v)
 
-print(instructions[0])
+	# replace position 1 with the value 12 and replace position 2 with the value 2
+	instructions[1] = noun
+	instructions[2] = verb
+
+	#loops through opcode positions
+	instructionpointer = 0
+	while instructionpointer < len(instructions) - 4:
+		# print(instructions[instructionpointer], instructions[instructionpointer+1], instructions[instructionpointer+2], instructions[instructionpointer+3])
+		if instructions[instructionpointer] != 99:
+			# print("not 99")
+			storeinpos = int(instructions[instructionpointer+3])
+			instructions = evalinstruction(instructions, instructionpointer)
+
+		instructionpointer += 4
+
+	# prints the answer
+	# print(instructions[0])
+	return instructions[0]
+
+if __name__ == '__main__':
+	# runinstructions(12,2)
+	print(100*76+21)
+	# for n in range(-100,100):
+	# 	for v in range(-100, 100):
+	# 		if runinstructions(n,v) == 19690720:
+	# 			print(n,v)
